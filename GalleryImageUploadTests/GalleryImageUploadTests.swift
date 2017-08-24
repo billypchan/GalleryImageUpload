de174d3102dc2c88465150960d849a6d6d130bc5
Bill Chan MBA <billypchan@gmail.com>
2017-08-24 16:38:56 +0200

    Initial Commit

---
+++ GalleryImageUploadTests/GalleryImageUploadTests.swift
@@ -0,0 +1,36 @@
+//
+//  GalleryImageUploadTests.swift
+//  GalleryImageUploadTests
+//
+//  Created by chan bill on 24/8/2017.
+//  Copyright © 2017 chan bill. All rights reserved.
+//
+
+import XCTest
+@testable import GalleryImageUpload
+
+class GalleryImageUploadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
