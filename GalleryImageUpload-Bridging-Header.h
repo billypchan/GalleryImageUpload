7bd7379be921fc6dc852fbf5a4b09665795d7a5b
Bill Chan MBA <billypchan@gmail.com>
2017-08-25 00:59:31 +0200

    bridge header for TOCropViewController

---
+++ GalleryImageUpload-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Firebase/Firebase.h>
+
+#import "TOCropViewController/TOCropViewController.h"
+#import "TOCropViewController/TOCropView.h"
+#import "TOCropViewController/TOCropToolbar.h"
+#import "TOCropViewController/TOCropOverlayView.h"
+
